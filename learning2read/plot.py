6126215e8e1393a3b808ba7311291236ca34d10d
nphard001 <nphard001@gmail.com>
2018-05-21 18:56:33 +0800

    ADD: plot.py

---
+++ learning2read/plot.py
@@ -0,0 +1,40 @@
+from pyecharts.base import Base
+import pandas as pd
+class PlotBase(Base): # my base :)
+    def __init__(self,w=600,h=400,*args,**kwargs):
+        super(PlotBase,self).__init__(width=w,height=h,*args,**kwargs)
+
+class SimpleScatter(PlotBase): # give (data,x,y) -> scatter
+    def setup(self,data,use_slider=False):
+        if isinstance(data,pd.DataFrame):
+            self.list_of_dict=data.to_dict("record")
+        else:
+            self.list_of_dict=data
+        self._option['xAxis']={'type': 'value'}
+        self._option['yAxis']={'type': 'value'}
+#         self._option['legend']=[{'data':[]}]
+        self._option['series']=[]
+        self._option['tooltip']={'show':True}
+        self._option['dataZoom']=[
+            {'type':'inside','xAxisIndex':0,'filterMode':'empty'},
+            {'type':'inside','yAxisIndex':0,'filterMode':'empty'}
+        ]
+        if use_slider:
+            self._option['dataZoom'].append({'type':'slider','xAxisIndex':0,'filterMode':'empty'})
+            self._option['dataZoom'].append({'type':'slider','yAxisIndex':0,'filterMode':'empty'})
+        return self
+    def add(self,xname="x",yname="y",fvalue=None,fsymbol=None,fsymbolSize=None,ftooltip=None):
+        fvalue = fvalue or (lambda r:[r[xname],r[yname]])
+        fsymbol= fsymbol or (lambda r:"circle")
+        fsymbolSize=fsymbolSize or (lambda r:10)
+        ftooltip=ftooltip or (lambda r:{'formatter':'(%.4f,%.4f)'%(r[xname],r[yname])})
+        new_series={}
+        new_series['type']="scatter"
+        new_series['data']=[{
+            'value':fvalue(r),
+            'symbol':fsymbol(r),
+            'symbolSize':fsymbolSize(r),
+            'tooltip':ftooltip(r),
+        } for r in self.list_of_dict]
+        self._option.get('series').append(new_series)
+        return self
