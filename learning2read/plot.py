622b22a9199106bdc532bd8e77c9fa0627b02f32
nphard001 <nphard001@gmail.com>
2018-05-24 16:03:22 +0800

    ADD: SimpleBar

--- learning2read/plot.py
+++ learning2read/plot.py
@@ -5,13 +5,17 @@ class PlotBase(Base): # my base :)
         super(PlotBase,self).__init__(width=w,height=h,*args,**kwargs)
 
 class SimpleScatter(PlotBase): # give (data,x,y) -> scatter
+    def setup_xAxis(self,atype="value"):
+        self._option['xAxis']={'type': atype}
+    def setup_yAxis(self,atype="value"):
+        self._option['yAxis']={'type': atype}
     def setup(self,data,use_slider=False):
         if isinstance(data,pd.DataFrame):
             self.list_of_dict=data.to_dict("record")
         else:
             self.list_of_dict=data
-        self._option['xAxis']={'type': 'value'}
-        self._option['yAxis']={'type': 'value'}
+        self.setup_xAxis()
+        self.setup_yAxis()
         self._option['legend']=[{'data':[]}]
         self._option['series']=[]
         self._option['tooltip']={'show':True}
@@ -37,14 +41,14 @@ class SimpleScatter(PlotBase): # give (data,x,y) -> scatter
             'tooltip':ftooltip(r),
         } for r in self.list_of_dict]
         if legend:
-            new_series['name']=new_series
+            new_series['name']=legend
             self._option.get('legend')[0].get('data').append(legend)
 
         self._option.get('series').append(new_series)
         return self
     @classmethod
     def demo(cls):
-        import math
+        import math # ugly
         return cls().setup([
             {'x':t*.1-10,'y':math.sin(t*.1-10)} for t in range(200)
         ],True).add(ftooltip=lambda r:{'formatter':'sin(%.4f)=%.4f'%(r['x'],r['y'])})
@@ -52,3 +56,17 @@ class SimpleScatter(PlotBase): # give (data,x,y) -> scatter
 class SimpleLine(SimpleScatter):
     def add(self,*args,**kwargs):
         return super(SimpleLine,self).add(series_type="line",*args,**kwargs)
+
+
+class SimpleBar(SimpleScatter):
+    def setup_xAxis(self,atype="category"):
+        self._option['xAxis']={'type': atype}
+    def add(self,*args,**kwargs):
+        return super(SimpleBar,self).add(series_type="bar",*args,**kwargs)
+    @classmethod
+    def demo(cls):
+        import random # ugly
+        import math   # ugly
+        return cls().setup([
+            {'x':i,'y':random.random(),'z':random.random()/2+0.25}
+        for i in range(20)]).add(legend="y").add(yname='z',legend="z")
