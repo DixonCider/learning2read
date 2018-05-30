44e413d2fbb61f5ce2f8f122114038cb2f625062
nphard001 <nphard001@gmail.com>
2018-05-30 13:18:19 +0800

    ADD: utils module

---
+++ learning2read/utils.py
@@ -0,0 +1,42 @@
+from difflib import ndiff
+from functools import reduce
+from path import Path # package "path.py"
+import pandas as pd
+import numpy as np
+import scipy
+
+def LCS(s1,s2): # length of LCS(s1,s2)
+    diff=ndiff(s1,s2)
+    result=0
+    for part in diff:
+        if part[0]==' ':
+            result+=1
+    return result
+    
+
+class DataLoader:
+    def __init__(self,path=r"/Users/qtwu/Downloads/data"):
+        self.path=path
+        self.file_list=[
+            "book_ratings_test.csv",
+            "book_ratings_train.csv",
+            "books.csv",
+            "implicit_ratings.csv",
+            "submission.csv",
+            "users.csv",
+        ]
+    def load(self,keyword,**kwargs):
+        filename_matched=reduce(
+            lambda a,b:a if DataLoader.better_match(keyword,a,b) else b,
+            self.file_list)
+        fpath=Path(self.path).joinpath(filename_matched)
+        print(fpath)
+        return pd.read_csv(fpath,**kwargs)
+    
+    @staticmethod
+    def better_match(key,s1,s2):
+        l1=LCS(key,s1)
+        l2=LCS(key,s2)
+        if l1==l2:
+            return len(s1)<len(s2)
+        return l1>l2
